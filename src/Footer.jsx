e0be57deb7ea048b2844086a5d39d458d46f2d5a
Diana Bishop <diana@bit.ly>
2022-01-17 13:30:04 -0500

    better edited pic of walter + paco

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,13 @@
 
 import React from 'react';
 import walter_and_paco from './assets/images/walter_and_paquito_edited.png';
+import walter_creep from './assets/images/walter_creeper_edited.png';
 
 
 const Footer = () => {
     return <div className="footer">
-        <img className="walter-and-paco-image " src={walter_and_paco} text-align="center" />
+        <img className="walter-and-paco-image" src={walter_and_paco} text-align="center" />
+        <img className="walter-creeper-image" src={walter_creep} text-align="center" />
     </div>
 }
 
