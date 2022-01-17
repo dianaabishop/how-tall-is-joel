2b84c46919f169c202e23e19aec0317a52ece83f
Diana Bishop <diana@bit.ly>
2022-01-16 20:24:55 -0500

    add walter and paco

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,11 @@
 
 import React from 'react';
+import walter_and_paco from './assets/images/walter_and_paquito_edited.png';
 
 
 const Footer = () => {
     return <div className="footer">
+        <img className="walter-and-paco-image " src={walter_and_paco} text-align="center" />
     </div>
 }
 
